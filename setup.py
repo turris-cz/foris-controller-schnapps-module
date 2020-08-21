fd2c10f7b882f286b5a772dc04426a1e4f755f63
Martin Matějek <martin.matejek@nic.cz>
2020-08-21 14:52:31 +0200

    setup.py: change url from gitlab.labs.nic.cz to gitlab.nic.cz

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #
 # foris-controller-schnapps-module
-# Copyright (C) 2019 CZ.NIC, z.s.p.o. (http://www.nic.cz/)
+# Copyright (C) 2019-2020 CZ.NIC, z.s.p.o. (http://www.nic.cz/)
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -43,13 +43,13 @@ setup(
     description=DESCRIPTION,
     long_description=open("README.rst").read(),
     install_requires=[
-        "foris-controller @ git+https://gitlab.labs.nic.cz/turris/foris-controller/foris-controller.git"
+        "foris-controller @ git+https://gitlab.nic.cz/turris/foris-controller/foris-controller.git"
     ],
     setup_requires=["pytest-runner"],
     tests_require=["pytest", "foris-controller-testtools", "foris-client", "ubus", "paho-mqtt"],
     dependency_links=[
-        "git+https://gitlab.labs.nic.cz/turris/foris-controller/foris-controller-testtools.git#egg=foris-controller-testtools",
-        "git+https://gitlab.labs.nic.cz/turris/foris-controller/foris-client.git#egg=foris-client",
+        "git+https://gitlab.nic.cz/turris/foris-controller/foris-controller-testtools.git#egg=foris-controller-testtools",
+        "git+https://gitlab.nic.cz/turris/foris-controller/foris-client.git#egg=foris-client",
     ],
     include_package_data=True,
     zip_safe=False,
